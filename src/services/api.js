5558d269e1882259cc81defe010f6f1ab4874da3
johnray214 <yujohnray96@gmail.com>
2025-10-13 11:08:28 +0800

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create axios instance
 const api = axios.create({
-   baseURL: 'http://127.0.0.1:8000/api',
+   baseURL: 'https://https://capstonebackend-production-ed22.up.railway.app/api',
    timeout: 60000,
    headers: {
       Accept: 'application/json',
